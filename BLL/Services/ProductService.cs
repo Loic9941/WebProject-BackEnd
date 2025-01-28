00ee0fbaab2cafdcb137089b76fd465585273c86
Loic994 <loic.marin.pro@gmail.com>
2025-01-28 21:57:10 +0100

    Add repo and First Service

---
+++ BLL/Services/ProductService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL.Models;
+using DAL.Repository;
+
+namespace BLL.Services
+{
+    public class ProductService
+    {
+        private readonly IRepository<Product> _productRepository;
+
+        public ProductService(IRepository<Product> productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+    }
+}
